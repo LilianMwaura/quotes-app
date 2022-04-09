ef99a8f4afea4c7e52b3cb6872ed722bc94b3be9
LilianMwaura <wambuimwaura28@gmail.com>
2022-04-09 15:30:57 +0300

    add math.max function to emit the highest upvotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,10 +10,12 @@ export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
     new Quote(1,"First quote" ,0 ,0),
-    new Quote(2, "Second quote" ,0 ,0),
+    new Quote(2, "Second quote" ,3 ,0),
     new Quote(3, "Third quote" ,0 ,0),
     new Quote(4, "Fourth quote" ,0 ,0)
   ]
+  arr : number [] = this.quotes.map(quote=>quote.upvotes)
+  highest = Math.max (...this.arr)
   constructor() { }
 
   ngOnInit(): void {
