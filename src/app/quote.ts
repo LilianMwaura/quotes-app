f93e11be4c44ba534f198268f940fd2ebd9306ae
LilianMwaura <wambuimwaura28@gmail.com>
2022-04-09 22:31:03 +0300

    create ngModel

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,7 @@
 
 export class Quote {
     showDescription: boolean;
-    constructor(public id: number, public name: string, public quote: string,public upvotes: number, public downvotes: number, public description: string,){
+    constructor(public id: number, public name: string, public quote: string,public upvotes: number, public downvotes: number, public description: string, public date: Date){
       this.showDescription=false;
     }
   } 
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,10 +9,10 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    new Quote(1, "Wambui","Sometimes it pays to stay in bed on Monday, rather than spending the rest of the week debugging Monday’s code." ,0 ,0, "-Martin fowler"),
-    new Quote(2, "Joe", "Always cod as if the person who ends up maintaining your code will be a violent psychopath who knows where you live." ,3 ,0,"-Oscar Wilde"),
-    new Quote(3, "Mweru","Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it." ,0 ,0,"-Francis Bacon"),
-    new Quote(4, "Hulda", "Software and cathedrals are much the same — first we build them, then we pray." ,0 ,0,"-Chris Heilmann")
+    new Quote(1, "Wambui","Sometimes it pays to stay in bed on Monday, rather than spending the rest of the week debugging Monday’s code." ,0 ,0, "-Martin fowler",new Date (2021,3,12)),
+    new Quote(2, "Joe", "Always code as if the person who ends up maintaining your code will be a violent psychopath who knows where you live." ,3 ,0,"-Oscar Wilde",new Date (2021,7,16)),
+    new Quote(3, "Mweru","Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it." ,0 ,0,"-Francis Bacon",new Date (2022,2,10)),
+    new Quote(4, "Hulda", "Software and cathedrals are much the same — first we build them, then we pray." ,0 ,0,"-Chris Heilmann",new Date (2020,4,9))
   ]
   arr : number [] = this.quotes.map(quote=>quote.upvotes)
   highest = Math.max (...this.arr)
