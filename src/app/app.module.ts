50d40cc45c9eb0d71482c28478b247faca5fea48
LilianMwaura <wambuimwaura28@gmail.com>
2022-04-09 19:16:59 +0300

    create pipe for date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { HighlightDirective } from './highlight.directive';
 import { QuoteDetailComponent } from './quote/quote-detail/quote-detail.component';
+import { DatePipe } from './date.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     HighlightDirective,
-    QuoteDetailComponent
+    QuoteDetailComponent,
+    DatePipe
   ],
   imports: [
     BrowserModule,
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,21 +1,7 @@
-// export class Quote {
-//     id!: number;
-//     quote!: string;
-//     upvotes!: number;
-//     downvotes!: number;
-//     description!: string;
-    
-//     constructor (id: number, quote: string, upvotes: number, downvotes: number,description:string){
-//         this.id = id;
-//         this.quote = quote;
-//         this.upvotes = upvotes;
-//         this.downvotes = downvotes;
-//         this.description = description
-//     }
-// }
+
 export class Quote {
     showDescription: boolean;
-    constructor(public id: number, public name: string, public quote: string,public upvotes: number, public downvotes: number, public author: string){
+    constructor(public id: number, public name: string, public quote: string,public upvotes: number, public downvotes: number, public description: string,){
       this.showDescription=false;
     }
   } 
