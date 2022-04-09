6d653d5e88579a1c0eb51801bda0c8b34616838d
LilianMwaura <wambuimwaura28@gmail.com>
2022-04-09 12:36:20 +0300

    add class components

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,17 @@
+import { stringify } from "querystring";
+
 export class Quote {
+    id: number;
+    quote: string;
+    upvotes: number;
+    downvotes: number;
+    
+    constructor (id: number, quote: string, upvotes: number, downvotes: number){
+        this.id = id;
+        this.quote = quote;
+        this.upvotes = upvotes;
+        this.downvotes = downvotes
+    }
 }
+    
+
